ea73b6354220581cc7a4e259628cbd2732af188e
BlackSheep <gaetanos31@hotmail.fr>
2023-09-04 18:05:29 +0200

    adding main

--- resources/js/utils/Styles/Page.jsx
+++ resources/js/utils/Styles/Page.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const PageStyle = styled.div`
+export const MainStyle = styled.main`
     width: 100%;
     min-height: 100vh;
     display: flex;
@@ -11,4 +11,4 @@ export const PageStyle = styled.div`
 
 
 
-export default PageStyle
+export default MainStyle
