21b52bb264552915eee76b7f9ba068e19641cd02
BlackSheep <gaetanos31@hotmail.fr>
2023-09-11 12:52:29 +0200

    calculate age

--- resources/js/Components/User/index.jsx
+++ resources/js/Components/User/index.jsx
@@ -19,51 +19,26 @@ const Vigilent = styled.span`
 
 function User({user}) {
 
+    const calculateAge = (birthdate) => {
+        const today = new Date();
+        const birthDate = new Date(birthdate);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+    
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+        }
+    
+        return age;
+      };
+    
+      const age = calculateAge('1995-10-31');
+
     return (
     <>
-        <div className="profile-wrapper" ref={user}>
-            {/* <Vigilent>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                </svg>
-
-                <span className="vigilent">Soyez vigilent ! Il y aura un quizz à la fin.</span>
-
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                </svg> 
-            </Vigilent> */}
-
-            {/* <div className="box-wrapper welcome-wrapper">
-
-                <div className='avatar'>
-                    <img className='test' src={Photo} alt='Gaetan' />
-                </div>
-
-                <div className="userdata-wrapper">
-                    {userInfo.map((info, index) => (
-                        <div className="data-wrapper" key={index} style={info.title === 'Email :' ? {marginRight: '5px'} : {}}>
-
-                            <div className="title-wrapper">
-                                {info.icon && <span className="icon">{info.icon}</span> }
-                                <span className="title">{ info.title }</span>
-                            </div>
-
-                            <div className="data-row">
-                                <span className="data">{info.data}</span>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-                <span className="confidentiel">*Vous trouverez mon email et numéro sur CV papier, par mesure de sécurité.</span>
-
-            </div> */}
-        </div>
-
-        <div className='user'>
+        <div className='user' ref={user}>
             <div className='user__desc'>
-                <h2>I'm Gaëtan ETAME, a <span>fullstack</span> web developpeur</h2>
+                <h2>I'm Gaëtan ETAME, i'm {age} and a <span>fullstack</span> web developpeur</h2>
                 <p>My goal is to help you to have the best website for your company and with the best experience for all the user.</p>
             </div>
 
