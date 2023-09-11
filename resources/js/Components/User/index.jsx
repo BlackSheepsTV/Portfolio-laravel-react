24581759f0242a4f05862f7b96d7d51823f61544
BlackSheep <gaetanos31@hotmail.fr>
2023-09-11 12:00:58 +0200

    adding user

--- resources/js/Components/User/index.jsx
+++ resources/js/Components/User/index.jsx
@@ -17,12 +17,12 @@ const Vigilent = styled.span`
 
 `
 
-function User() {
+function User({user}) {
 
     return (
-    
-        <div className="profile-wrapper">
-            <Vigilent>
+    <>
+        <div className="profile-wrapper" ref={user}>
+            {/* <Vigilent>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                 </svg>
@@ -32,9 +32,9 @@ function User() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                 </svg> 
-            </Vigilent>
+            </Vigilent> */}
 
-            <div className="box-wrapper welcome-wrapper">
+            {/* <div className="box-wrapper welcome-wrapper">
 
                 <div className='avatar'>
                     <img className='test' src={Photo} alt='Gaetan' />
@@ -58,9 +58,20 @@ function User() {
 
                 <span className="confidentiel">*Vous trouverez mon email et numéro sur CV papier, par mesure de sécurité.</span>
 
+            </div> */}
+        </div>
+
+        <div className='user'>
+            <div className='user__desc'>
+                <h2>I'm Gaëtan ETAME, a <span>fullstack</span> web developpeur</h2>
+                <p>My goal is to help you to have the best website for your company and with the best experience for all the user.</p>
             </div>
 
+            <div className='avatar'>
+                <img src={Photo} alt='Gaetan'/>
+            </div>
         </div>
+    </>
     )
 }
 
