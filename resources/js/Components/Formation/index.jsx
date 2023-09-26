6b94d893c39534bc9072ecfaab4b254f8c2599d6
BlackSheep <gaetanos31@hotmail.fr>
2023-09-26 11:06:17 +0200

    formation dynamic date

--- resources/js/Components/Formation/index.jsx
+++ resources/js/Components/Formation/index.jsx
@@ -6,12 +6,23 @@ function Formation() {
 
     const { currentDate } = useDate()
 
+    const end = new Date('2023-10-08')
+
+    function compareDate() {
+       
+        if(currentDate < end) {
+            return currentDate.toLocaleDateString()
+        } else {
+            return end.toLocaleDateString()
+        }
+    }
+
     return (
         <div className="box-wrapper box-parcours-wrapper">
             {formations.map((formation, index) => (
                 <div className='parcour-row' key={index}>
                     <div className='date-wrapper'>
-                        <span className='date'>{formation.date} {index === 0 ? currentDate.toLocaleDateString() : ''}</span> <span className=''>{formation.ecole}</span>
+                        <span className='date'>{formation.date} {index === 0 ?  compareDate() : '' }</span> <span className=''>{formation.ecole}</span>
                     </div>
                     <span className='objectif'>{formation.etude}</span>
                 </div>
